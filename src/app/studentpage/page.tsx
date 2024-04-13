5b55149db8f12bf333d9d2a4e0ca6a9b2b799c96
Madina <madinaamankeldinova5@gmail.com>
2024-04-14 04:40:36 +0500

    Modify student page and user profile styles, add calendar component

--- src/app/studentpage/page.tsx
+++ src/app/studentpage/page.tsx
@@ -1,10 +1,12 @@
 import Navbar from "@/components/Navbar"
 import UserProfile from "@/components/UserProfile";
+import Calendar from "@/components/Calendar";
 
 export default function Home() {
     return (
         <>
         <Navbar />
+        <Calendar />
         </>
     );
 

a6f84b79a6ed335648a21f80f37f15060d1da6c9
Madina <madinaamankeldinova5@gmail.com>
2024-04-14 06:24:04 +0500

    added component for attendance tracking probnik tests

--- src/app/studentpage/page.tsx
+++ src/app/studentpage/page.tsx
@@ -3,12 +3,19 @@ import UserProfile from "@/components/UserProfile";
 import Calendar from "@/components/Calendar";
 import ExamCountdown from "@/components/ExamCount";
 import AttendanceTracker from "@/components/AttendanceTracker";
+import TestRes from "@/components/TestRes";
 
 export default function Home() {
     return (
         <>
         <Navbar />
+        
+        <div className="flex w-1/3">
         <AttendanceTracker/>
+        <TestRes/>
+
+        </div>
+        
         </>
     );
 
---
+++ src/components/TestRes.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const TestRes = () => {
+  const squares = Array.from({ length: 12 }, (_, index) => (
+    <div
+      key={index}
+      className={`w-8 h-8 ${Math.random() > 0.3 ? 'bg-blue-500' : 'bg-gray-300'} mr-2`}
+    />
+  ));
+
+  return (
+    <div className="bg-gray-100 p-4 rounded-lg shadow-md">
+      <h2 className="text-lg font-semibold mb-4">Attented tests in the past 3 month</h2>
+      <div className="flex mb-2">
+        {squares}
+      </div>
+    </div>
+  );
+};
+
+export default TestRes;

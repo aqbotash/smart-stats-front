c0e5d4f4bb6aa243bf16d1b0fd88507ce61258f6
Madina <madinaamankeldinova5@gmail.com>
2024-04-14 05:14:45 +0500

    Update student page, add ExamCountdown component and styles

--- src/app/studentpage/page.tsx
+++ src/app/studentpage/page.tsx
@@ -1,12 +1,13 @@
 import Navbar from "@/components/Navbar"
 import UserProfile from "@/components/UserProfile";
 import Calendar from "@/components/Calendar";
+import ExamCountdown from "@/components/ExamCount";
 
 export default function Home() {
     return (
         <>
         <Navbar />
-        <Calendar />
+        <ExamCountdown />
         </>
     );
 
